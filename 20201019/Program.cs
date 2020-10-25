709a0ec2b274b8e792ddd0a0172cb27d70cc3142
NoZ-design <68496572+NoZ-design@users.noreply.github.com>
2020-10-25 23:03:46 +0800

    20201019HW

---
+++ 20201019/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _20201019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int keyin; 
+            int number1; 
+            int number2; 
+            int answer;
+
+            Random rm = new Random();
+            number1 = rm.Next(0, 10);
+            number2 = rm.Next(0, 10);
+
+            do
+            {
+                Console.Write("How much is {0} times {1}？", number1, number2);
+                keyin = int.Parse(Console.ReadLine());
+                answer = number1 * number2;
+
+                if(keyin == answer)
+                {
+                    Console.WriteLine("Very good.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("No.Please try again.");
+                }
+            }
+            while (true);
+
+            Console.Read();
+        }
+    }
+}
